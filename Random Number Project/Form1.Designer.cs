3ed2bee1173af9b035a93c3f3db5bfe05a699ea0
zaharra24 <zaharrachami@gmail.com>
2025-07-18 22:30:55 -0400

    Random Number Game
    A C# console game where the user guesses a random number between 1 to 100. The program provides feedback ("higher" or "lower") after each game and users can request a hint.

---
+++ Random Number Project/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Random_Number_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.lblFeedback = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnHint = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(78, 24);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(177, 13);
+            this.lblInstructions.TabIndex = 0;
+            this.lblInstructions.Text = "Enter a number between 1 and 100:";
+            // 
+            // lblFeedback
+            // 
+            this.lblFeedback.AutoSize = true;
+            this.lblFeedback.Location = new System.Drawing.Point(147, 62);
+            this.lblFeedback.Name = "lblFeedback";
+            this.lblFeedback.Size = new System.Drawing.Size(20, 13);
+            this.lblFeedback.TabIndex = 1;
+            this.lblFeedback.Text = "\" \"";
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(113, 97);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(100, 20);
+            this.txtGuess.TabIndex = 2;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnGuess.Location = new System.Drawing.Point(138, 155);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(75, 23);
+            this.btnGuess.TabIndex = 3;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Brown;
+            this.btnExit.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnExit.Location = new System.Drawing.Point(128, 210);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnHint
+            // 
+            this.btnHint.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnHint.Location = new System.Drawing.Point(12, 155);
+            this.btnHint.Name = "btnHint";
+            this.btnHint.Size = new System.Drawing.Size(75, 23);
+            this.btnHint.TabIndex = 5;
+            this.btnHint.Text = "Hint";
+            this.btnHint.UseVisualStyleBackColor = true;
+            this.btnHint.Click += new System.EventHandler(this.btnHint_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnHelp.Location = new System.Drawing.Point(277, 155);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(75, 23);
+            this.btnHelp.TabIndex = 6;
+            this.btnHelp.Text = "Help";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(592, 333);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnHint);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblFeedback);
+            this.Controls.Add(this.lblInstructions);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Random Number Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Label lblFeedback;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnHint;
+        private System.Windows.Forms.Button btnHelp;
+    }
+}
+
---
+++ Random Number Project/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random_Number_Project
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        int randomNumber;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            randomNumber = random.Next(1, 101);
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            int userGuess;
+
+            if (int.TryParse(txtGuess.Text, out userGuess))
+            {
+                if (userGuess >=1 && userGuess <= 100)
+                {
+                    if (userGuess < randomNumber)
+                    {
+                        lblFeedback.Text = "Too low!";
+                    }
+                    else if (userGuess > randomNumber)
+                    {
+                        lblFeedback.Text = "Too high!";
+                    }
+                    else
+                    {
+                        lblFeedback.Text = "Correct! Well done!";
+
+                        DialogResult result = MessageBox.Show("Do you want to play again?", "Play Again?", MessageBoxButtons.YesNo);
+                        if (result == DialogResult.Yes)
+                        {
+                            randomNumber = random.Next(1, 101);
+                            lblFeedback.Text = "";
+                            txtGuess.Clear();
+                        }
+                    }
+                }
+                else
+                {
+                    lblFeedback.Text = "Invalid input. Please enter a valid number.";
+
+                }
+                txtGuess.Clear();
+                txtGuess.Focus();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnHint_Click(object sender, EventArgs e)
+        {
+            if (randomNumber % 2 ==0)
+            {
+                lblFeedback.Text = "Hint: The number is even.";
+            }
+            else
+            {
+                lblFeedback.Text = "Hint: The number is odd.";
+            }
+            if (randomNumber > 50)
+            {
+                lblFeedback.Text = "Hint: The number is greater than 50.";
+            }
+            else
+            {
+                lblFeedback.Text = "Hint: The number is less than or equal to 50.";
+            }
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            lblFeedback.Text = "Enter a number between 1 and 100. Use the Guess button to submit your guess.";
+        }
+    }
+}
